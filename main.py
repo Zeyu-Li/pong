d6e79b91a2d9137b6755576fe93dd6e49091d738
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-10-16 20:39:06 -0600

    Moved things arround

--- main.py
+++ main.py
@@ -87,13 +87,13 @@ def run(tick_speed, window_size):
         window_size.fill(black)
 
         # draws walls
-        walls(window_size)
+        Objects(window_size).walls()
 
         # draws player paddle
-        paddle(window_size, x_p, y_p)
+        Objects(window_size, x_p, y_p).paddle()
 
         # draws ball
-        ball(window_size, center, y_p, ball_speed)
+        Objects(window_size, x_p, y_p, center, ball_speed).ball()
 
         # updates display
         pygame.display.update()
@@ -103,74 +103,83 @@ def run(tick_speed, window_size):
         clock.tick(tick_speed)
 
 
-def ball(window_size, center, y_p, speed = [scale, scale]):
-    # draws ball
-    # - window_size = size of window
-    # - center = center of ball
-    # - y_p = the height of paddle
-    # - speed = speed of a ball as a list. 
-    # First number is the x speed, second is the y speed, default = [scale, scale]
+class Objects():
 
-    # NOTE: +3 is ~ the radius of ball
+    def __init__(self, window_size, x_p = 0, y_p = 0, center = 0, speed = [scale, scale]):
+        self.window_size = window_size
+        self.center = center
+        self.y_p = y_p
+        self.x_p = x_p
+        self.speed = speed
 
-    # if the center of ball plus speed plus the radius of ball is greater 
-    # than the wall's position, reverse the speed of the ball
-    if center[0] + speed[0] + 3 > true_window_w - 2*scale:
-        speed[0] = -speed[0]
-    elif center[0] - 2*scale - 6 < 0:
-        # tests to see if it hits the paddle. If it doesn't quit game
-        if y_p > center[1] - 2*scale - 6 or y_p + int(window_h * .1) < center[1] - 2*scale - 3:
-            pygame.quit()
-            sys.exit()
-        # else, take the position that it hit the paddle and calculate a degree at which it should bounce back
-        # the more out of the center it is, the more vertical it becomes
-        else:
-            degrees = (center[1] - 2*scale - 6 - y_p)/(window_h * .1) * 110 + 35
-            speed[0] = round(cos(degrees)*sqrt(50))
-            speed[1] = round(sin(degrees)*sqrt(50))
-        speed[0] = -speed[0]
-    if center[1] + speed[1] + 3 > true_window_h - 2*scale:
-        speed[1] = -speed[1]
-    elif center[1] - 2*scale - 6 < 0:
-        speed[1] = -speed[1]
+    def ball(self):
+        # draws ball
+        # - window_size = size of window
+        # - center = center of ball
+        # - y_p = the height of paddle
+        # - speed = speed of a ball as a list. 
+        # First number is the x speed, second is the y speed, default = [scale, scale]
 
-    # calculates next position and draws it
-    center[0] += speed[0]
-    center[1] += speed[1]
-    circle = pygame.draw.circle(window_size, white, center, scale)
+        # NOTE: +3 is ~ the radius of ball
 
-def walls(window_size):
-    # draws the walls of the board
-    # - window_size = size of window
+        # if the center of ball plus speed plus the radius of ball is greater 
+        # than the wall's position, reverse the speed of the ball
+        if self.center[0] + self.speed[0] + 3 > true_window_w - 2*scale:
+            self.speed[0] = -self.speed[0]
+        elif self.center[0] - 2*scale - 6 < 0:
+            # tests to see if it hits the paddle. If it doesn't quit game
+            if self.y_p > self.center[1] - 2*scale - 6 or self.y_p + int(window_h * .1) < self.center[1] - 2*scale - 3:
+                pygame.quit()
+                sys.exit()
+            # else, take the position that it hit the paddle and calculate a degree at which it should bounce back
+            # the more out of the center it is, the more vertical it becomes
+            else:
+                degrees = (self.center[1] - 2*scale - 6 - self.y_p)/(window_h * .1) * 110 + 35
+                self.speed[0] = round(cos(degrees)*sqrt(50))
+                self.speed[1] = round(sin(degrees)*sqrt(50))
+            self.speed[0] = -self.speed[0]
+        if self.center[1] + self.speed[1] + 3 > true_window_h - 2*scale:
+            self.speed[1] = -self.speed[1]
+        elif self.center[1] - 2*scale - 6 < 0:
+            self.speed[1] = -self.speed[1]
 
-    # sets the wall width and height
-    wall_w = window_w
-    wall_h = scale
+        # calculates next position and draws it
+        self.center[0] += self.speed[0]
+        self.center[1] += self.speed[1]
+        circle = pygame.draw.circle(self.window_size, white, self.center, scale)
 
-    # draws the top and bottom wall
-    pygame.draw.rect(
-        window_size, white, (2*scale, scale, wall_w, wall_h)
-    )
-    pygame.draw.rect(
-        window_size, white, (2*scale, scale+window_h, wall_w, wall_h)
-    )
+    def walls(self):
+        # draws the walls of the board
+        # - window_size = size of window
+
+        # sets the wall width and height
+        wall_w = window_w
+        wall_h = scale
+
+        # draws the top and bottom wall
+        pygame.draw.rect(
+            self.window_size, white, (2*scale, scale, wall_w, wall_h)
+        )
+        pygame.draw.rect(
+            self.window_size, white, (2*scale, scale+window_h, wall_w, wall_h)
+        )
 
 
-def paddle(window_size, x_p, y_p):
-    # draws the player paddle
-    # - window_size = size of window
-    # - x_p = the x position of the paddle
-    # - y_p = the y position of the paddle
+    def paddle(self):
+        # draws the player paddle
+        # - window_size = size of window
+        # - x_p = the x position of the paddle
+        # - y_p = the y position of the paddle
 
-    # TODO: rounded paddles like css border-radius
-    # defines the height and width of the paddle
-    height_p = int(window_h * .1)
-    width_p = scale
+        # TODO: rounded paddles like css border-radius
+        # defines the height and width of the paddle
+        height_p = int(window_h * .1)
+        width_p = scale
 
-    # draws the player paddle
-    pygame.draw.rect(
-        window_size, white, (x_p, y_p, width_p, height_p)
-    )
+        # draws the player paddle
+        pygame.draw.rect(
+            self.window_size, white, (self.x_p, self.y_p, width_p, height_p)
+        )
 
 # calls main function
 main()
